001799aebc27405d09fe60d4438af37999e67e65
phavichy <nadphavich@gmail.com>
2023-01-29 05:40:13 +0700

    0540 290123

--- sched_runprocess.py
+++ sched_runprocess.py
@@ -4,4 +4,3 @@ from process import sched_process
 # Import PDF
 pdf_files = glob.glob('*.pdf')
 df_all, df_dep, midnight_flt, df_passive, df_date, df_vertical, df_final, df_final_block = sched_process(pdf_files)
-print(df_final_block.to_string())
